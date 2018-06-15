f3a73108f07e2aaffb14ea754f62d85c45e0a48c
Suprit Bhattacharjee <supritbhattacharjee@Suprits-MacBook-Air.local>
2018-06-15 18:55:46 +0530

    Rectified error

--- ml_code.py
+++ ml_code.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 def main():
-    cancer = load_breast_cancer
+    cancer = load_breast_cancer()
     X_train,X_test,y_train,y_test = train_test_split(cancer["data"],cancer["target"],random_state=0)
     clf = LogisticRegression().fit(X_train,y_train)
     print("Training score data: %f" % clf.score(X_train,y_train))
